8669f876821107e69d91b165015aad4cc5affb43
george <georgij-net@yandex.ru>
2018-01-02 02:00:31 +0300

    add game brain-calc

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,6 +1,28 @@
+import _ from 'lodash';
+
+const sign = {
+  '+': (a, b) => a + b,
+  '-': (a, b) => a - b,
+  '*': (a, b) => a * b,
+};
+
+const generatePositiveInt = () => _.random(0, 100);
+
+const generateSign = (signObj) => {
+  const signs = Object.keys(signObj);
+  const index = _.random(0, signs.length - 1);
+  return signs[index];
+};
+
 const generate = () => {
-  const question = 1;
-  const correct = 1;
+  const number1 = generatePositiveInt();
+  const number2 = generatePositiveInt();
+  const randomSign = generateSign(sign);
+
+  const question = `${number1} ${randomSign} ${number2}`;
+  const result = sign[randomSign](number1, number2);
+  const correct = String(result);
+
   return { question, correct };
 };
 
