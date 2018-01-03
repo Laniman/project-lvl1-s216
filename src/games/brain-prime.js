cd102b021a812670bedcc28f91fd75d9eba5d133
george <georgij-net@yandex.ru>
2018-01-03 17:26:13 +0300

    add game logic for game brain-prime

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,8 +1,25 @@
 import { run } from '..';
+import { generateInt } from '../utils';
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+
+  const rec = (i) => {
+    if (i === num) {
+      return true;
+    }
+    return num % i === 0 ? false : rec(i + 1);
+  };
+
+  return rec(2);
+};
 
 const generate = () => {
-  const question = '';
-  const correct = '';
+  const number = generateInt();
+  const question = number;
+  const correct = isPrime(number) ? 'yes' : 'no';
   return { question, correct };
 };
 
