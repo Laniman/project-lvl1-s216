d251a464ab11af8113002badb0d87b6394e687d0
george <georgij-net@yandex.ru>
2018-01-03 16:48:48 +0300

    add logic for game brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,11 +1,35 @@
+import _ from 'lodash';
 import { run } from '..';
 
-const logic = () => {};
+const makeProgressionTerms = (initialTerm, difference, progressionLength) => {
+  const rec = (terms) => {
+    if (terms.length === progressionLength) {
+      return terms.slice().reverse();
+    }
+
+    const [head] = terms;
+    return rec([head + difference, ...terms]);
+  };
+
+  return rec([initialTerm]);
+};
+
+const makeQuestion = (progression, missingTermIndex, missingTerm) =>
+  progression
+    .map((term, index) => (index === missingTermIndex ? missingTerm : term))
+    .join(' ');
+
 
 const generate = () => {
-  const question = '';
-  const result = logic();
-  const correct = String(result);
+  const initialTerm = _.random(0, 10);
+  const difference = _.random(0, 10);
+  const progressionLength = 10;
+  const missingTerm = '..';
+  const missingTermIndex = _.random(0, progressionLength);
+
+  const progression = makeProgressionTerms(initialTerm, difference, progressionLength);
+  const question = makeQuestion(progression, missingTermIndex, missingTerm);
+  const correct = String(progression[missingTermIndex]);
 
   return { question, correct };
 };
