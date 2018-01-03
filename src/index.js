206ab9fb431c1ee31a523d6066cb3a35ff84f394
george <georgij-net@yandex.ru>
2018-01-03 15:25:46 +0300

    refactor multilayer structures

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import readlineSync from 'readline-sync';
+import _ from 'lodash';
 
-const CORRECT_ANSWER_NUMBER = 3;
-const CORRECT_ANSWER_MESSAGE = 'Correct!';
-const ANSWER_MESSAGE = 'Your answer: ';
+const correctAnswerNumber = 3;
+const correctAnswerMessage = 'Correct!';
+const answerMessage = 'Your answer: ';
+const defaultDescriptionMessage = 'No description\n';
 
 const makeQuestionMessage = question => `Question: ${question}`;
 const makeTryAgainMessage = name => `Let's try again, ${name}!`;
@@ -10,9 +12,6 @@ const makeSuccessGameMessage = name => `Congratulations, ${name}!`;
 const makeWrongMessage = (wrong, correct) => `'${wrong}' is wrong answer ;(. Correct answer was '${correct}'.`;
 const print = console.log;
 const welcome = () => print('Welcome to the Brain Games!');
-const getDescription = game => game.description;
-const getGenerator = game => game.generate;
-const isGame = game => game.generate && typeof game.generate === 'function';
 
 export const greet = () => {
   const userName = readlineSync.question('May I have your name? ');
@@ -22,14 +21,13 @@ export const greet = () => {
 
 export const run = (game) => {
   welcome();
-  if (!isGame(game)) {
+  const { generate, description = defaultDescriptionMessage } = game;
+  if (_.isUndefined(generate)) {
     print();
     greet();
     return;
   }
 
-  const generate = getGenerator(game);
-  const description = getDescription(game);
   print(description);
   const name = greet();
 
@@ -41,16 +39,16 @@ export const run = (game) => {
 
     const { question, correct } = generate();
     print(makeQuestionMessage(question));
-    const answer = readlineSync.question(ANSWER_MESSAGE);
+    const answer = readlineSync.question(answerMessage);
     const isCorrectAnswer = answer === correct;
     if (!isCorrectAnswer) {
       print(makeWrongMessage(answer, correct));
       print(makeTryAgainMessage(name));
       return;
     }
-    print(CORRECT_ANSWER_MESSAGE);
+    print(correctAnswerMessage);
     inner(count - 1);
   };
 
-  inner(CORRECT_ANSWER_NUMBER);
+  inner(correctAnswerNumber);
 };
